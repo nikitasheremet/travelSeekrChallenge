7e0fc125626b10e8088dde1a90d5e82dbda7f437
Nikita Sheremet <nikita.sheremet253@gmail.com>
2019-11-21 08:06:42 -0500

    refactor flight cards in flight tab

---
+++ App/FlightItem.js
@@ -0,0 +1,128 @@
+import React from "react";
+
+import { StyleSheet, Text, View, Image } from "react-native";
+
+export default function FlightItem({ item }) {
+  return (
+    <View style={{ width: 300, height: 350 }}>
+      {Object.values(item)
+        .slice(1)
+        .map((flightDirection, index) => {
+          return (
+            <View
+              key={index}
+              style={{
+                borderColor: "white",
+                borderWidth: 1,
+                borderRadius: "10px",
+                margin: 10,
+                padding: 10,
+                backgroundColor: "white",
+                shadowColor: "#000",
+                shadowOffset: { width: 2, height: 2 },
+                shadowOpacity: 0.2,
+                shadowRadius: 8
+              }}
+            >
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  borderBottomColor: "lightgrey",
+                  borderBottomWidth: 1,
+                  alignItems: "center",
+                  paddingBottom: 10
+                }}
+              >
+                <Text style={{ marginRight: 5, fontSize: 12 }}>
+                  {flightDirection.direction}
+                </Text>
+                <Text>•</Text>
+                <Text style={{ marginLeft: 5, fontSize: 12 }}>
+                  {flightDirection.date}
+                </Text>
+              </View>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  marginTop: 15,
+                  marginBottom: 15
+                }}
+              >
+                <Image
+                  source={{ uri: flightDirection.airlineIcon }}
+                  style={{
+                    width: 30,
+                    height: 30,
+                    marginRight: 10,
+                    marginLeft: 5
+                  }}
+                />
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    marginRight: 15
+                  }}
+                >
+                  <Text style={{ fontWeight: "bold" }}>
+                    {flightDirection.departureTime}
+                  </Text>
+                  <Text>{flightDirection.departureAirportCode}</Text>
+                </View>
+                <Image
+                  source={require("./right-arrow.png")}
+                  style={{ width: 20, height: 20 }}
+                />
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    marginLeft: 20
+                  }}
+                >
+                  <Text style={{ fontWeight: "bold" }}>
+                    {flightDirection.arrivalTime}
+                  </Text>
+                  <Text>{flightDirection.arrivalAirportCode}</Text>
+                </View>
+              </View>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-between"
+                }}
+              >
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center"
+                  }}
+                >
+                  <Text style={{ fontSize: 10, marginRight: 3 }}>
+                    {flightDirection.numStops}
+                  </Text>
+                  <Text style={{ marginRight: 3 }}>•</Text>
+                  <Text style={{ fontSize: 10, marginRight: 3 }}>
+                    {flightDirection.duration}
+                  </Text>
+                  <Text style={{ marginRight: 3 }}>•</Text>
+                  <Text style={{ fontSize: 10 }}>
+                    {flightDirection.airlineName}
+                  </Text>
+                </View>
+                <Text style={{ fontWeight: "bold" }}>
+                  {flightDirection.price}
+                </Text>
+              </View>
+            </View>
+          );
+        })}
+    </View>
+  );
+}
--- App/FlightTab.js
+++ App/FlightTab.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
   View,
   TouchableHighlight,
   FlatList,
-  Image
+  Image,
+  Modal
 } from "react-native";
 
+import FlightItem from "./FlightItem";
+
 export default function FlightTab({ tabClicked, setTabClicked }) {
+  const [modalVisible, setModalVisible] = useState(false);
   const DATA = [
     {
       id: "1",
@@ -104,226 +108,7 @@ export default function FlightTab({ tabClicked, setTabClicked }) {
       }
     }
   ];
-  function Item({ item }) {
-    return (
-      <View style={{ width: 300, height: 350 }}>
-        <View
-          style={{
-            borderColor: "white",
-            borderWidth: 1,
-            borderRadius: "10px",
-            margin: 10,
-            padding: 10,
-            backgroundColor: "white",
-            shadowColor: "#000",
-            shadowOffset: { width: 2, height: 2 },
-            shadowOpacity: 0.2,
-            shadowRadius: 8
-          }}
-        >
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              borderBottomColor: "lightgrey",
-              borderBottomWidth: 1,
-              alignItems: "center",
-              paddingBottom: 10
-            }}
-          >
-            <Text style={{ marginRight: 5, fontSize: 12 }}>
-              {item.departing.direction}
-            </Text>
-            <Text>•</Text>
-            <Text style={{ marginLeft: 5, fontSize: 12 }}>
-              {item.departing.date}
-            </Text>
-          </View>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              marginTop: 15,
-              marginBottom: 15
-            }}
-          >
-            <Image
-              source={{ uri: item.departing.airlineIcon }}
-              style={{
-                width: 30,
-                height: 30,
-                marginRight: 10,
-                marginLeft: 5
-              }}
-            />
-            <View
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                marginRight: 15
-              }}
-            >
-              <Text style={{ fontWeight: "bold" }}>
-                {item.departing.departureTime}
-              </Text>
-              <Text>{item.departing.departureAirportCode}</Text>
-            </View>
-            <Image
-              source={require("./right-arrow.png")}
-              style={{ width: 20, height: 20 }}
-            />
-            <View
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                marginLeft: 20
-              }}
-            >
-              <Text style={{ fontWeight: "bold" }}>
-                {item.departing.arrivalTime}
-              </Text>
-              <Text>{item.departing.arrivalAirportCode}</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between"
-            }}
-          >
-            <View
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center"
-              }}
-            >
-              <Text style={{ fontSize: 10, marginRight: 3 }}>
-                {item.departing.numStops}
-              </Text>
-              <Text style={{ marginRight: 3 }}>•</Text>
-              <Text style={{ fontSize: 10, marginRight: 3 }}>
-                {item.departing.duration}
-              </Text>
-              <Text style={{ marginRight: 3 }}>•</Text>
-              <Text style={{ fontSize: 10 }}>{item.departing.airlineName}</Text>
-            </View>
-            <Text style={{ fontWeight: "bold" }}>{item.departing.price}</Text>
-          </View>
-        </View>
-        <View
-          style={{
-            borderColor: "white",
-            borderWidth: 1,
-            borderRadius: "10px",
-            margin: 10,
-            padding: 10,
-            backgroundColor: "white",
-            shadowColor: "#000",
-            shadowOffset: { width: 2, height: 2 },
-            shadowOpacity: 0.2,
-            shadowRadius: 8
-          }}
-        >
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              borderBottomColor: "lightgrey",
-              borderBottomWidth: 1,
-              alignItems: "center",
-              paddingBottom: 10
-            }}
-          >
-            <Text style={{ marginRight: 5, fontSize: 12 }}>
-              {item.returning.direction}
-            </Text>
-            <Text>•</Text>
-            <Text style={{ marginLeft: 5, fontSize: 12 }}>
-              {item.returning.date}
-            </Text>
-          </View>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              marginTop: 15,
-              marginBottom: 15
-            }}
-          >
-            <Image
-              source={{ uri: item.returning.airlineIcon }}
-              style={{
-                width: 30,
-                height: 30,
-                marginRight: 10,
-                marginLeft: 5
-              }}
-            />
-            <View
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                marginRight: 15
-              }}
-            >
-              <Text style={{ fontWeight: "bold" }}>
-                {item.returning.departureTime}
-              </Text>
-              <Text>{item.returning.departureAirportCode}</Text>
-            </View>
-            <Image
-              source={require("./right-arrow.png")}
-              style={{ width: 20, height: 20 }}
-            />
-            <View
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                marginLeft: 20
-              }}
-            >
-              <Text style={{ fontWeight: "bold" }}>
-                {item.returning.arrivalTime}
-              </Text>
-              <Text>{item.returning.arrivalAirportCode}</Text>
-            </View>
-          </View>
-          <View
-            style={{
-              display: "flex",
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between"
-            }}
-          >
-            <View
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "center"
-              }}
-            >
-              <Text style={{ fontSize: 10, marginRight: 3 }}>
-                {item.returning.numStops}
-              </Text>
-              <Text style={{ marginRight: 3 }}>•</Text>
-              <Text style={{ fontSize: 10, marginRight: 3 }}>
-                {item.returning.duration}
-              </Text>
-              <Text style={{ marginRight: 3 }}>•</Text>
-              <Text style={{ fontSize: 10 }}>{item.returning.airlineName}</Text>
-            </View>
-            <Text style={{ fontWeight: "bold" }}>{item.returning.price}</Text>
-          </View>
-        </View>
-      </View>
-    );
-  }
+
   return (
     <View
       style={{
@@ -341,7 +126,8 @@ export default function FlightTab({ tabClicked, setTabClicked }) {
             marginLeft: 30,
             alignItems: "center",
             marginTop: 20,
-            justifyContent: "space-between"
+            justifyContent: "space-between",
+            marginBottom: 15
           }}
         >
           <View
@@ -373,10 +159,16 @@ export default function FlightTab({ tabClicked, setTabClicked }) {
             <Text style={{ fontSize: 14, fontWeight: "bold", marginRight: 10 }}>
               C$ 650
             </Text>
-            <Image
-              source={require("./three-dots.png")}
-              style={{ width: 15, height: 15, backgroundColor: "#F1F1F1" }}
-            />
+            <TouchableHighlight
+              onPress={() => {
+                setModalVisible(true);
+              }}
+            >
+              <Image
+                source={require("./three-dots.png")}
+                style={{ width: 15, height: 15, backgroundColor: "#F1F1F1" }}
+              />
+            </TouchableHighlight>
           </View>
         </View>
       </TouchableHighlight>
@@ -391,7 +183,7 @@ export default function FlightTab({ tabClicked, setTabClicked }) {
           style={{
             width: "100%",
             paddingLeft: 20,
-            marginTop: 20
+            marginTop: 10
           }}
         >
           <View
@@ -461,12 +253,26 @@ export default function FlightTab({ tabClicked, setTabClicked }) {
             <FlatList
               horizontal
               data={DATA}
-              renderItem={({ item }) => <Item item={item} />}
+              renderItem={({ item }) => <FlightItem item={item} />}
               keyExtractor={item => item.id}
             />
           </View>
         </View>
       </View>
+      <Modal
+        animationType="fade"
+        transparent={false}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(!modalVisible)}
+      >
+        <View style={{ padding: 50, display: "flex", alignItems: "flex-end" }}>
+          <View>
+            <TouchableHighlight onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={{ fontSize: 20 }}>X</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
